f70968ddf26bedc0ef79bb18ee6d3711367ce4d0
Yoan Marchal <ymarchal@rnd.fr>
2022-11-27 13:41:16 +0100

    swith to nuxt 3 & nuxt content

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('D:/projects/blog-netlify-nuxt/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
+  const parseContent: typeof import('D:/projects/blog-netlify-nuxt/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
--- content/blog/2022-06-05-test-content.md
+++
@@ -1,6 +0,0 @@
----
-title: test content
-date: 2022-06-05T17:25:08.736Z
-description: Best team ever
----
-1. ![super maillot ](/img/b1.png "best team ever")
