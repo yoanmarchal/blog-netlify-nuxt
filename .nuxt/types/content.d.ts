af2af5eb64d3feab8525de26ed525e42fad904da
Yoan Marchal <marchalyoan@gmail.com>
2022-11-27 16:31:01 +0100

    RM nuxt files

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('D:/projects/blog-netlify-nuxt/node_modules/@nuxt/content/dist/runtime/server').serverQueryContent
-  const parseContent: typeof import('D:/projects/blog-netlify-nuxt/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
