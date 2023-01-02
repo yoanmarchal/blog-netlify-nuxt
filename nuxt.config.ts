aad5d19e54ce3e1760a169bbcd2836fb60f3d532
Yoan Marchal <marchalyoan@gmail.com>
2023-01-02 12:46:46 +0100

    clean up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,8 @@ export default defineNuxtConfig({
   components: true,
   modules: [
     '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxtjs/pwa'
+    '@nuxt/ui'
   ],
-  plugins: ['~/plugins/preview.client.js'],
   content: {
     documentDriven: true
   }
