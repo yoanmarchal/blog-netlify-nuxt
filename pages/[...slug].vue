3bd28c5070ec46de7e5478629ce2853cf9823b56
Yoan Marchal <marchalyoan@gmail.com>
2023-01-29 16:38:54 +0100

    updata deps

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,11 +1,14 @@
 <template>
-  <main class="prose">
-    <ContentDoc>
-      <template #not-found>
-        <h1>Document not found</h1>
-      </template>
-      <template #empty>
-      </template>
-    </ContentDoc>
-  </main>
+  <NuxtLayout>
+    <main>
+      <ContentDoc>
+        <template #not-found>
+          <h1>Document not found</h1>
+        </template>
+        <template #empty>
+        </template>
+      </ContentDoc>
+    </main>
+  </NuxtLayout>
+
 </template>
